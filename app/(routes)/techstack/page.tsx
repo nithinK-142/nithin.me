e2cf7598529212be99e272de53046720bd70b257
nithinK-142 <nithin.sagar.359@gmail.com>
2023-09-22 19:23:31 +0530

    add: more tech-stack icons removed redundant tech-stack code.

--- app/(routes)/techstack/page.tsx
+++ app/(routes)/techstack/page.tsx
@@ -1,78 +1,39 @@
+import React from "react";
 import { backend, frontend, tools, hosting } from "../../data/tech-stack";
 
-const TechStackPage = () => {
+interface TechStackItem {
+  logo: JSX.Element;
+  label: string;
+}
+
+const TechStackPage: React.FC = () => {
+  const generateTechStack = (techArray: TechStackItem[], title: string) => (
+    <div>
+      <h4 className="text-lg font-medium mb-2">{title}</h4>
+      <div className="flex gap-2 flex-wrap items-center text-sm">
+        {techArray.map(({ logo, label }, index) => (
+          <div
+            className="h-9 w-auto flex items-center gap-2 bg-stone-900 py-2 px-3 rounded select-none"
+            key={index}
+            title={label}
+          >
+            <div className="h-4">{logo}</div>
+            <p>{label}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
   return (
-    <section className="mt-8">
-      {/* <h3 className="text-2xl uppercase font-semibold mb-2">Tech Stack</h3> */}
-
+    // <section className="mt-8">
+    // <h3 className="text-2xl uppercase font-semibold mb-2">Tech Stack</h3>
+    <section>
       <div className="grid grid-cols-2 gap-x-8 gap-y-4">
-        <div>
-          <h4 className="text-lg font-medium mb-2">Frontend</h4>
-
-          <div className="flex gap-2 flex-wrap items-center text-sm">
-            {frontend.map(({ logo, label }, index) => (
-              <div
-                className="h-9 w-auto flex items-center gap-2 bg-stone-900 py-2 px-3 rounded select-none"
-                key={index}
-                title={label}
-              >
-                <div className="h-4">{logo}</div>
-                <p>{label}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-
-        <div>
-          <h4 className="text-lg font-medium mb-2">Backend</h4>
-
-          <div className="flex gap-2 flex-wrap items-center text-sm">
-            {backend.map(({ logo, label }, index) => (
-              <div
-                className="h-9 w-auto flex items-center gap-2 bg-stone-900 py-2 px-3 rounded select-none"
-                key={index}
-                title={label}
-              >
-                <div className="h-4">{logo}</div>
-                <p>{label}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-
-        <div>
-          <h4 className="text-lg font-medium mb-2">Tools</h4>
-
-          <div className="flex gap-2 flex-wrap items-center text-sm">
-            {tools.map(({ logo, label }, index) => (
-              <div
-                className="h-9 w-auto flex items-center gap-2 bg-stone-900 py-2 px-3 rounded select-none"
-                key={index}
-                title={label}
-              >
-                <div className="h-4">{logo}</div>
-                <p>{label}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-        
-        <div>
-          <h4 className="text-lg font-medium mb-2">Hosting</h4>
-
-          <div className="flex gap-2 flex-wrap items-center text-sm">
-            {hosting.map(({ logo, label }, index) => (
-              <div
-                className="h-9 w-auto flex items-center gap-2 bg-stone-900 py-2 px-3 rounded select-none"
-                key={index}
-                title={label}
-              >
-                <div className="h-4">{logo}</div>
-                <p>{label}</p>
-              </div>
-            ))}
-          </div>
-        </div>
+        {generateTechStack(frontend, 'Frontend')}
+        {generateTechStack(backend, 'Backend')}
+        {generateTechStack(tools, 'Tools')}
+        {generateTechStack(hosting, 'Hosting')}
       </div>
     </section>
   );
