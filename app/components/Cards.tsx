c6ff979189f7939678cc9d524e4d729733f4aae9
nithinK-142 <nithin.sagar.359@gmail.com>
2023-09-22 10:14:54 +0530

    fix: projects gitlinks and added scaling.

--- app/components/Cards.tsx
+++ app/components/Cards.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { main } from "../data/projects";
 import { Icons } from "../data/icons";
 
-const Cards = ({ href, title, description, technologies }: main) => (
+const Cards = ({ href, gitLink, title, description, technologies }: main) => (
   <div className="group rounded-lg border border-transparent px-2 sm:px-3 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 dark:border-neutral-700 hover:dark:bg-neutral-800/30">
     <h2 className="mb-3 text-xl font-semibold group-hover:underline">
       <a href={href} className="" target="_blank" rel="noopener noreferrer">
@@ -13,8 +13,8 @@ const Cards = ({ href, title, description, technologies }: main) => (
       </span>
       {/* <span className="opacity-0 group-hover:opacity-100"> */}
       <span>
-        <a href={href} target="_blank" rel="noopener noreferrer">
-          <button className="h-4 pl-1 fill-white">{Icons.github}</button>
+        <a href={gitLink} target="_blank" rel="noopener noreferrer">
+          <button className="h-4 pl-1 fill-white hover:scale-125 duration-300">{Icons.github}</button>
         </a>
       </span>
     </h2>
