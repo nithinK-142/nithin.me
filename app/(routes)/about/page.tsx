787caf3b943bdabd2ebe29730666216a855d9cdb
nithinK-142 <nithin.sagar.359@gmail.com>
2023-09-24 11:39:46 +0530

    update: improved bio.

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -8,43 +8,55 @@ const AboutPage = () => {
     <section className="md:mb-6">
       <div className="space-y-4 font-sand">
         <div className="leading-relaxed">
-          <p>
-            I&apos;m <span className="font-medium">Nithin,</span> a 23 years old
-            self-taught <span className="font-medium">Web Developer</span> / a{" "}
-            <span className="font-medium">Tech Nerd</span> living in{" "}
+          <p className="block pt-4 lg:hidden">
+            Passionate & proficient in Front-End technologies such as HTML5,
+            CSS3, Tailwind, ReactJs, NextJS and SvelteKit apart from this, I
+            have good knowledge in PHP & MySQL for Back-End. Focused on
+            user-friendly interfaces & continuous learning to stay ahead in the
+            ever-evolving realm of web. Collaborative team player & dedicated to
+            staying updated on the latest trends.
+          </p>
+          <p className="about-para">
+            I&apos;m <span>Nithin,</span> a self-taught{" "}
+            <span>Web Developer</span> / <span>Tech Nerd</span> from{" "}
             <a
-              href="https://en.wikipedia.org/wiki/Bangalore"
+              href="https://en.wikipedia.org/wiki/Shimoga"
               target="_blank"
               rel="noopener noreferrer"
-              className="font-medium underline underline-offset-4"
+              className="about-link hover:text-blue-500"
             >
-              Bangalore, India
-            </a>
-            {". "}
-            I&apos;ve recently completed my
-            <span className="font-medium">
+              Shimoga, India
+            </a>{" "}
+            with a
+            <span>
               {" "}
-              Master&apos;s in Compter Applications at{" "}
+              Master&apos;s Degree{" "}
+              <abbr
+                className="text-lg hover:text-blue-500"
+                title="Master of Computer Applications"
+              >
+                &#40;MCA&#41;
+              </abbr>{" "}
+              from{" "}
               <a
                 href="https://www.cambridge.edu.in/"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="font-medium underline underline-offset-4"
+                className="about-link"
               >
                 Cambridge Institute of Technology
               </a>{" "}
               - Bangalore, India
               {". "}
             </span>
-            I have extensive knowledge of HTML, CSS & JavaScript and I&apos;m
-            passionate about building blazing fast, user-friendly, responsive
-            and visually stunning web applications with cutting-edge
-            technologies like{" "}
+            I&apos;m Proficient in HTML, CSS & JavaScript and passionate about
+            building blazing fast, user-friendly, responsive and visually
+            stunning web applications with cutting-edge technologies like{" "}
             <a
               href="https://nextjs.org"
               target="_blank"
               rel="noopener noreferrer"
-              className="font-medium underline underline-offset-4"
+              className="about-link"
             >
               NextJs
             </a>{" "}
@@ -53,7 +65,7 @@ const AboutPage = () => {
               href="https://svelte.dev"
               target="_blank"
               rel="noopener noreferrer"
-              className="font-medium underline underline-offset-4"
+              className="about-link"
             >
               SvelteKit
             </a>{" "}
@@ -62,24 +74,30 @@ const AboutPage = () => {
               href="https://tailwindcss.com/"
               target="_blank"
               rel="noopener noreferrer"
-              className="font-medium underline underline-offset-4"
+              className="about-link"
             >
               TailwindCSS
             </a>
             .
           </p>
-          <p className="hidden pt-2 lg:block">
-            I spend hours experimenting with HTML, CSS, and JavaScript or
-            dabbling with new technologies. I&apos;m curious and enjoy work that
-            challenges me to learn something new and stretch in a different
-            direction. Outside of coding, I enjoy spending time reading novels,
-            listening to music and podcasts, gaming or binge-watching.
+          <p className="about-para">
+            I spend a lot of time messing around with{" "}
+            <abbr
+              className="text-lg hover:text-blue-500"
+              title="HTML, CSS & JavaScript"
+            >
+              web
+            </abbr>{" "}
+            and checking out latest tech trends. I&apos;m super curious and love
+            tackling projects that make me learn something new and take me in
+            new directions. When I&apos;m not coding, I enjoy spending time
+            jamming to music and podcasts, gaming, or just binge-watching stuff.
           </p>
           <p>
             I&apos;m currently available for a new freelance job.{" "}
             <a
               href="https://www.fiverr.com/nithin_webdev?up_rollout=true"
-              className="font-medium underline underline-offset-4"
+              className="about-link"
               target="_blank"
               rel="noreferrer"
             >
@@ -87,12 +105,12 @@ const AboutPage = () => {
             </a>
           </p>
         </div>
-        <div className="flex flex-col gap-8 sm:flex-row sm:gap-0">
+        <div className="gap-8 flex-colm sm:flex-row sm:gap-0">
           <div className="w-full sm:w-1/2">
-            <p className="my-4 text-xl font-semibold uppercase">Achievements</p>
+            <p className="about-extra">Achievements</p>
             <ul className="space-y-1">
               {achivements.map(({ title, link }, index) => (
-                <li className="flex items-center gap-2" key={index}>
+                <li className="gap-2 flex-align" key={index}>
                   <span className="h-4 fill-green-500">{icons.tick}</span>
                   <a
                     href={link}
@@ -107,13 +125,15 @@ const AboutPage = () => {
             </ul>
           </div>
 
-          <div className="pb-4">
-            <p className="my-4 text-xl font-semibold uppercase">
-              My happy pills
-            </p>
+          <div className="hidden pb-4 md:block">
+            <p className="about-extra">My happy pills</p>
             <div className="flex gap-3">
               {happyPills.map(({ platform, icon }) => (
-                <span title={platform} key={platform} className="hover:animate-spinning">
+                <span
+                  title={platform}
+                  key={platform}
+                  className="hover:animate-spinning"
+                >
                   {icon}
                 </span>
               ))}
