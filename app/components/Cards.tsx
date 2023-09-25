4bd35c3862bbd0b720e763a1d20efab17f75abba
nithinK-142 <nithin.sagar.359@gmail.com>
2023-09-25 12:11:54 +0530

    update: project cards bg change.

--- app/components/Cards.tsx
+++ app/components/Cards.tsx
@@ -3,7 +3,7 @@ import { MainProjectsType } from "../data/projects";
 import { socialIcons } from "../data/social-icons";
 
 const Cards = ({ href, gitLink, title, description, technologies }: MainProjectsType) => (
-  <div className="px-2 py-4 transition-colors border border-transparent rounded-lg group sm:px-3 hover:border-gray-300 hover:bg-gray-100 dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+  <div className="px-2 py-4 transition-colors border rounded-lg border-gray-100/30 bg-black/30 group sm:px-3 hover:border-gray-300">
     <h2 className="mb-3 text-xl font-semibold group-hover:underline">
       <a href={href} target="_blank" rel="noopener noreferrer">
         {title}{" "}
@@ -18,7 +18,7 @@ const Cards = ({ href, gitLink, title, description, technologies }: MainProjects
         </a>
       </span>
     </h2>
-    <p className="m-0 md:max-w-[30ch] text-sm opacity-50">{description}</p>
+    <p className="m-0 md:max-w-[30ch] text-sm opacity-70">{description}</p>
 
     <div className="flex flex-wrap justify-center gap-2 mt-2">
       {technologies.map((tech, index) => (
