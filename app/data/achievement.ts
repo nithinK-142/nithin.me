d474819d37113fddaaf53a1bf1455324d4774a2e
nithinK-142 <nithin.sagar.359@gmail.com>
2023-09-26 17:20:13 +0530

    update: achievement rename.

--- app/data/achievement.ts
+++ app/data/achievement.ts
@@ -9,7 +9,7 @@ export const achivements: AchivementsType[] = [
     link: "https://www.freecodecamp.org/certification/fcc12be2f4c-b687-4cd2-852f-bb33961a1103/responsive-web-design",
   },
   {
-    title: "HackerRank Certifications",
+    title: "Certifications @HackerRank",
     link: "https://www.hackerrank.com/certificates/1035b3bc7b40",
   },
 ];
