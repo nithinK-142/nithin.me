6a8f5dd3799329a4329da337abcd1399e7e42ea9
nithinK-142 <nithin.sagar.359@gmail.com>
2023-09-26 15:55:06 +0530

    update: custom scrollbar to persist app layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,10 +27,10 @@ export default function RootLayout({
         <link rel="icon" type="image/x-icon" href="favicon.svg" />
       </head>
       <body className={`${inter.className} bg-[#1f1f1f] text-white font-medium`}>
-        <div className="flex justify-center w-full ">
+        <div className="flex justify-center w-full">
           <div className="h-screen w-[90%] sm:w-[70%] flex flex-col justify-between py-1 sm:py-2 px-1 sm:px-2">
             <Navbar />
-            <main>{children}</main>
+            <main className=" floating-scrollbar">{children}</main>
             <Footer />
           </div>
         </div>
