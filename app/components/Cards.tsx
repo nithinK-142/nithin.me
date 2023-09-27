77405e282dd319cbe4deee0bfa28fe376da5899e
nithinK-142 <nithin.sagar.359@gmail.com>
2023-09-27 23:09:05 +0530

    update: hover effect for project cards.

--- app/components/Cards.tsx
+++ app/components/Cards.tsx
@@ -3,7 +3,7 @@ import { MainProjectsType } from "../data/projects";
 import { socialIcons } from "../data/social-icons";
 
 const Cards = ({ href, gitLink, title, description, technologies }: MainProjectsType) => (
-  <div className="px-2 py-4 transition-colors border rounded-lg border-gray-100/30 bg-black/30 group sm:px-3 hover:border-gray-300">
+  <div className="px-2 py-4 border rounded-lg hover:rounded-none transition-all duration-700 border-gray-100/30 bg-black/30 group sm:px-3 hover:border-gray-300">
     <h2 className="mb-3 text-xl">
       <a href={href} target="_blank" rel="noopener noreferrer" className="underlined font-semibold pb-1">
         {title}{" "}
