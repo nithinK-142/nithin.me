e4239050e15403c3429f3c2aa17d1ad2a801a0d4
nithinK-142 <nithin.sagar.359@gmail.com>
2023-09-29 10:19:42 +0530

    update: shadwo for button.

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({ buttonText, fontWeight = "bold", animation }) => {
   return (
     <button
-      className={`${animation} mt-5 px-4 py-2 sm:mx-auto bg-[#f05f45] text-white font-${fontWeight} text-md rounded-md`}
+      className={`${animation} hover:shadow-[0_20px_50px_rgba(240,_95,_69,_0.7)] mt-5 px-4 py-2 sm:mx-auto bg-[#f05f45] text-white font-${fontWeight} text-md rounded-md`}
     >
       {buttonText}
     </button>
