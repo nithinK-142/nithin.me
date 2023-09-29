fa5dc06895cbb57c3982a581daac0565046c0fab
nithinK-142 <nithin.sagar.359@gmail.com>
2023-09-29 20:51:32 +0530

    update: techstack page enhancement.

--- app/(routes)/techstack/page.tsx
+++ app/(routes)/techstack/page.tsx
@@ -8,9 +8,9 @@ interface TechStackItem {
 
 const TechStackPage: React.FC = () => {
   const techStackCard = (techArray: TechStackItem[], title: string) => (
-    <div>
-      <h4 className="mb-2 text-lg font-semibold">{title}</h4>
-      <div className="flex flex-wrap items-center gap-2 text-sm">
+    <div className="bg-black/10 mb-4">
+      <h4 className="mb-2 text-lg font-semibold text-center">{title}</h4>
+      <div className="flex flex-wrap items-center justify-center gap-2 text-sm">
         {techArray.map(({ logo, label }, index) => (
           <div
             className="flex items-center w-auto gap-2 px-3 py-2 duration-200 rounded select-none hover:scale-110 h-9 bg-stone-950"
@@ -29,7 +29,7 @@ const TechStackPage: React.FC = () => {
     // <section className="mt-8">
     // <h3 className="mb-2 text-2xl font-semibold uppercase">Tech Stack</h3>
     <section className="my-16 md:my-0">
-      <div className="grid grid-cols-2 gap-x-8 gap-y-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-4">
         {techStackCard(frontend, 'Frontend')}
         {techStackCard(backend, 'Backend')}
         {techStackCard(tools, 'Tools')}
