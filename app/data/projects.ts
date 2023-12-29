366fb508e0720ff6f3a456c2a440b6872c0a4d99
nithinK-142 <nithin.sagar.359@gmail.com>
2023-12-29 12:36:29 +0530

    update: projects.

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -11,17 +11,29 @@ export const mainProjects: MainProjectsType[] = [
     href: "https://nithin-me.vercel.app/",
     gitLink: "https://github.com/nithinK-142/nithin.me",
     title: "Portfolio V3",
-    description:
-      "Single page app with clean and simple layout.",
-    technologies: ["NextJs", "Tailwind","Typescript"],
+    description: "Single page app with clean and simple layout.",
+    technologies: ["NextJs", "Tailwind", "Typescript"],
   },
   {
     href: "https://next-aesop-clone.vercel.app/",
     gitLink: "https://github.com/nithinK-142/next-aesop-clone",
     title: "Aesop Clone",
-    description:
-      "Clone of aesop made using nextjs13.",
-    technologies: ["NextJs", "Tailwind","Typescript"],
+    description: "Clone of aesop made using nextjs13.",
+    technologies: ["NextJs", "Tailwind", "Typescript"],
+  },
+  {
+    href: "https://mern-cart.vercel.app/",
+    gitLink: "https://github.com/nithinK-142/mern-cart",
+    title: "MERN Cart",
+    description: "A simple ECommerce app using MERN Stack.",
+    technologies: ["ReactJs", "TypeScript", "NodeJs", "ExpressJS", "Mongodb"],
+  },
+  {
+    href: "https://unsplash-gallery-app.vercel.app/",
+    gitLink: "https://github.com/nithinK-142/unsplash-gallery",
+    title: "Unsplash Gallery",
+    description: "Gallery app with Carousal and Modal.",
+    technologies: ["ReactJs", "React-Hooks", "Tailwind", "Unsplash API"],
   },
   {
     href: "https://sveltekittodo.netlify.app/",
@@ -31,25 +43,10 @@ export const mainProjects: MainProjectsType[] = [
     technologies: ["SvelteKit", "Firebase", "Firebase-Auth", "Tailwind"],
   },
   {
-    href: "https://unsplashgalleryapp.netlify.app/",
-    gitLink: "https://github.com/nithinK-142/unsplash-gallery",
-    title: "Unsplash Gallery",
-    description: "Gallery app with Carousal and Modal.",
-    technologies: ["ReactJs", "React-Hooks", "Tailwind", "Unsplash API"],
-  },
-  {
-    href: "https://sveltetodos.netlify.app/",
-    gitLink: "https://github.com/nithinK-142/todo-svelte-fire",
-    title: "Svelte Todo",
-    description: "Add, Complete , Delete and Repeat..",
-    technologies: ["Svelte", "Firebase", "Tailwind"],
-  },
-  {
-    href: "https://nithin-dev.netlify.app/",
+    href: "https://nithink-142.github.io/",
     gitLink: "https://github.com/nithinK-142/nithink-142.github.io",
     title: "Portfolio V2",
-    description:
-      "Crafted using React with a clean and elegant design..",
+    description: "Crafted using React with a clean and elegant design..",
     technologies: ["ReactJs", "Bootstrap"],
   },
   {
@@ -63,8 +60,7 @@ export const mainProjects: MainProjectsType[] = [
     href: "https://nasaapiusage.netlify.app/",
     gitLink: "https://github.com/nithinK-142/NASA-API-usage",
     title: "Space Explorer",
-    description:
-      "Space exploring with nasa. Images of outer space.",
+    description: "Space exploring with nasa. Images of outer space.",
     technologies: ["HTML", "CSS", "JS"],
   },
 ];
@@ -77,14 +73,14 @@ type OtherType = {
 export const otherProjects: OtherType[] = [
   {
     title: "nithinK-142/vue-expense-tracker",
-    href: "https://github.com/nithinK-142/vue-expense-tracker"
+    href: "https://github.com/nithinK-142/vue-expense-tracker",
   },
   {
     title: "nithinK-142/spaceportfolio",
-    href: "https://github.com/nithinK-142/spaceportfolio"
+    href: "https://github.com/nithinK-142/spaceportfolio",
   },
   {
-    title: "nithinK-142/tmdb-movies",
-    href: "https://github.com/nithinK-142/tmdb-movies"
+    title: "nithinK-142/svelte-todo",
+    href: "https://github.com/nithinK-142/todo-svelte-fire",
   },
 ];
