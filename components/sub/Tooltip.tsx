65904f9f52a5527cec8819f8c722d63eac575c03
nithinK-142 <nithin.sagar.359@gmail.com>
2024-01-05 13:29:22 +0530

    fix: transition of tooltiptext.

--- components/sub/Tooltip.tsx
+++ components/sub/Tooltip.tsx
@@ -17,10 +17,10 @@ const tooltipStyles = {
 
 const Tooltip = ({ text, content, tooltip = "normal" }: TooltipProps) => {
   return (
-    <span className="dark:text-white group relative hover:opacity-90 font-semibold inline-block transition-slow">
+    <span className="dark:text-white group relative hover:opacity-90 font-semibold inline-block">
       {text}
       <span
-        className={`absolute bg-black dark:bg-white whitespace-nowrap text-white dark:text-black font-semibold text-sm rounded-lg p-2 group-hover:inline-block hidden transform-center transition-all duration-300 origin-top ${tooltipStyles[tooltip].body}`}
+        className={`absolute bg-black dark:bg-white whitespace-nowrap text-white dark:text-black font-semibold text-sm rounded-lg p-2 group-hover:inline-block hidden transform-center origin-top ${tooltipStyles[tooltip].body}`}
         aria-label="tooltip"
         role="tooltip"
       >
