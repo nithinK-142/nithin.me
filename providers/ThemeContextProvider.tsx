4dc3ad46ada100e4c9030565879764b9c6aa441b
nithinK-142 <nithin.sagar.359@gmail.com>
2024-01-15 18:41:47 +0530

    update: ThemeProvider defaulttheme from system to dark.

--- providers/ThemeContextProvider.tsx
+++ providers/ThemeContextProvider.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from "next-themes";
 
 export function ThemeContextProvider({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
       {children}
     </ThemeProvider>
   );
