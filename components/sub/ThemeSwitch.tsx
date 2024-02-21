700c1e2e321e21d1c4d2247de0f6f8ded54d7f9c
nithinK-142 <nithin.sagar.359@gmail.com>
2024-02-21 10:26:05 +0530

    refactor: theme switch placeholder dimension.

--- components/sub/ThemeSwitch.tsx
+++ components/sub/ThemeSwitch.tsx
@@ -15,12 +15,13 @@ const ThemeSwitch = () => {
     return (
       <Image
         src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
-        width={32}
-        height={32}
-        sizes="32x32"
+        width={28}
+        height={28}
+        sizes="28x28"
         alt="Loading Light/Dark Toggle"
         priority={false}
         title="Loading Light/Dark Toggle"
+        className="pt-0.5"
       />
     );
 
