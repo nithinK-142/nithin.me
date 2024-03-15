5c55c4fe9dc8bd31a0471c03c72dbb7ec112122a
nithinK-142 <nithin.sagar.359@gmail.com>
2024-03-15 14:33:25 +0530

    feat: projectpage new look.

--- app/(routes)/projects/page.tsx
+++ app/(routes)/projects/page.tsx
@@ -1,55 +1,46 @@
-import Cards from "./Cards";
-import { mainProjects, otherProjects, frameworkGuides } from "@/data/projects";
+// import Cards from "./Cards";
+// import { mainProjects, otherProjects, frameworkGuides } from "@/data/projects";
+// import Header from "@/components/Header";
+// import OtherProjects from "./OtherProjects";
+// import FrameworkGuide from "./FrameworkGuide";
+// import { AnimationProvider } from "@/providers/AnimationProvider";
+// import {
+//   slideInFromLeft,
+//   slideInFromRight,
+//   slideInFromTop,
+// } from "@/util/motion";
 import "@/styles/scrollbar.css";
-import Header from "@/components/Header";
-import OtherProjects from "./OtherProjects";
-import FrameworkGuide from "./FrameworkGuide";
-import { AnimationProvider } from "@/providers/AnimationProvider";
-import {
-  slideInFromLeft,
-  slideInFromRight,
-  slideInFromTop,
-} from "@/util/motion";
+import { projects } from "@/data/projects-new";
+import Link from "next/link";
+import { icons } from "@/app/assets/icons";
 
 const ProjectsPage = () => {
   return (
-    <AnimationProvider initial="hidden" animate="visible" className="my-16">
-      <Header heading="Projects" />
-      <AnimationProvider
-        varients={slideInFromTop()}
-        className="grid text-center xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 lg:gap-4 md:gap-3 sm:gap-2 custom-scrollbar"
-      >
-        {mainProjects.map((link, index) => (
-          <Cards key={index} {...link} />
-        ))}
-      </AnimationProvider>
-      <div className="flex flex-col-reverse md:flex-row md:justify-between mt-6">
-        <AnimationProvider varients={slideInFromLeft}>
-          <p className="text-xl">Other Noteworthy Projects</p>
-          <ul className="pb-2 ml-4">
-            {otherProjects.map(({ title, href }, index) => (
-              <OtherProjects key={index} title={title} href={href} />
-            ))}
-          </ul>
-        </AnimationProvider>
-        <AnimationProvider
-          varients={slideInFromRight}
-          className="mb-6 md:mr-4 lg:mr-20"
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4 ">
+      {projects.map(({ name, description, logo, url }, index) => (
+        <div
+          className="flex flex-col justify-between hover:bg-neutral-100 transition-colors ease-in-out duration-300 p-4 select-none group border border-black/10 dark:border-stone-900 dark:hover:bg-neutral-900 dark:hover:shadow-lg hover:shadow-lg"
+          key={index}
         >
-          <p className="text-xl">Framework Guides</p>{" "}
-          <ul className="pb-2 ml-2 floating-scrollbar">
-            {frameworkGuides.map(({ title, href, framework }, index) => (
-              <FrameworkGuide
-                key={index}
-                title={title}
-                framework={framework}
-                href={href}
-              />
-            ))}
-          </ul>
-        </AnimationProvider>
-      </div>
-    </AnimationProvider>
+          <div className="pt-2">
+            <div className="h-12 ml-0 block mr-auto">{logo}</div>
+            <h4 className="text-xl font-semibold mt-4 mb-1">{name}</h4>
+            <p className="text-sm">{description}</p>
+          </div>
+
+          <div className="flex items-center gap-2 mt-6 font-medium text-sm group-hover:text-blue-700 dark:group-hover:text-yellow-500 transition-colors ease-in-out duration-300">
+            {icons.link}
+            <Link
+              href={`https://${url}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {url}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 };
 
