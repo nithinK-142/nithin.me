315cd38629d88a22ffe7b70f369fd641d3d4f19e
nithinK-142 <nithin.sagar.359@gmail.com>
2024-03-15 21:58:24 +0530

    refactor: navbar ui.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Inter } from "next/font/google";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import ThemeSwitch from "./ThemeSwitch";
@@ -7,22 +8,26 @@ import { navItems } from "@/data/nav-items";
 import { icons } from "@/app/assets/icons";
 import { navsmall } from "@/app/assets/nav-small";
 
+const inter = Inter({
+  weight: ["400", "500", "600"],
+  subsets: ["latin"],
+});
+
 const Navbar = () => {
   const pathname = usePathname();
-
   return (
-    <nav
-      className={`flex-justify gap-2 lg:pr-2 md:gap-0 md:justify-between relative`}
-    >
+    <nav className="flex-justify gap-2 lg:pr-2 md:gap-0 md:justify-between relative">
       {pathname !== "/" ? (
-        <div className="nav-text hidden md:block">
+        <div className="hidden md:block">
           <div className="justify-center gap-2 flex-align">
-            <div className="w-12 h-12 pt-[2px]">
+            <div className="w-10 h-10 pt-[2px]">
               <Link href="/">{icons.user}</Link>
             </div>
 
             <Link href="/">
-              <h2 className="text-xl font-bold tracking-[.1em]">Nithin</h2>
+              <h2 className="text-lg h-6 font-semibold tracking-[.1em]">
+                NITHIN
+              </h2>
               <p className="font-mono text-sm font-semibold opacity-80">
                 @nithinK-142
               </p>
@@ -39,54 +44,50 @@ const Navbar = () => {
         </div>
       )}
 
-      <div className="hidden md:flex flex-wrap gap-2 sm:gap-4 mb-3">
-        <p
-          className={`block md:hidden nav-text pt-1 ${
-            pathname === "/" ? "hidden" : "opacity-70"
-          }`}
-        >
-          <Link href="/" className="font-semibold" passHref>
-            Home
-          </Link>
-        </p>
+      <ul className="hidden md:flex flex-wrap gap-2 sm:gap-4 mb-3">
         {navItems.map((item) => {
           const isActive = pathname === item.path;
           return (
-            <div
+            <li
               key={item.path}
-              className={`nav-text pt-1 ${
-                isActive ? "opacity-100" : "hover:opacity-90 opacity-70"
-              }`}
+              className={`${
+                inter.className
+              } text-base tracking-wide font-normal pt-0.5
+              ${isActive ? "opacity-100" : "hover:opacity-90 opacity-70"}`}
             >
               {isActive && (
                 <div className="flex-justify items-center">
                   <span className="opacity-0 md:opacity-100 arrow-head"></span>
                 </div>
               )}
-              <Link href={item.path} className="font-semibold" passHref>
+              <div className="py-0.5"></div>
+              <Link href={item.path} passHref>
                 {item.label}
               </Link>
-            </div>
+            </li>
           );
         })}
-      </div>
-      <div className="hidden md:block absolute -right-8 top-[2px] cursor-pointer">
+      </ul>
+      <div className="hidden md:block absolute -right-8 cursor-pointer">
         <ThemeSwitch />
       </div>
 
       {/* Small Screen Nav */}
-      <div className="md:hidden pt-2 flex flex-justify items-center flex-wrap max-xs:space-x-4 space-x-8">
+      <ul className="md:hidden pt-2 flex flex-justify items-center flex-wrap max-xs:space-x-4 space-x-8">
         {navsmall.map((nav, index) => (
-          <Link key={index} href={nav.path}>
-            <div className="fill-black dark:fill-white opacity-80 dark:opacity-90">
+          <li key={index}>
+            <Link
+              href={nav.path}
+              className="fill-black dark:fill-white opacity-80 dark:opacity-90"
+            >
               {nav.logo}
-            </div>
-          </Link>
+            </Link>
+          </li>
         ))}
-        <div className="cursor-pointer">
+        <li className="cursor-pointer">
           <ThemeSwitch />
-        </div>
-      </div>
+        </li>
+      </ul>
     </nav>
   );
 };
